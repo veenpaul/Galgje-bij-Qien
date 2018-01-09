e9dd13167d5a7c1d077deda8702bc430bbfe03b9
Paul <p_veen@live.nl>
2018-01-09 13:39:27 +0100

    wordt getoond

--- Galgje.java
+++ Galgje.java
@@ -1,7 +1,12 @@
 
 public class Galgje {
 public static void main (String[] args) {
-System.out.println("Hallo");
-System.out.println("Die is veranderd");
+System.out.println("Welkom BijGalgje");
+System.out.println("Het te raden woord:");
+
+String teRadenWoord = "Qien";
+
+System.out.println(teRadenWoord);
+
 }
 }
